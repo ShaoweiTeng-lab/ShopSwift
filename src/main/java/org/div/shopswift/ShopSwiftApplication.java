81506ebda8b91f2361f1b2bc47595567a108a234
Jason_Teng <sdonline13@gmail.com>
2024-02-28 11:50:39 +0800

    first commit

---
+++ src/main/java/org/div/shopswift/ShopSwiftApplication.java
@@ -0,0 +1,13 @@
+package org.div.shopswift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopSwiftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopSwiftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/div/shopswift/ShopSwiftApplicationTests.java
@@ -0,0 +1,13 @@
+package org.div.shopswift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopSwiftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
