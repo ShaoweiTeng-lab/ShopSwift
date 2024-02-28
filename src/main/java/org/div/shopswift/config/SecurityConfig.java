a4b0613cb1ae20997500a7fb84e285a1238e9e6f
Jason_Teng <sdonline13@gmail.com>
2024-02-28 12:02:07 +0800

    add properties

---
+++ src/main/java/org/div/shopswift/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package org.div.shopswift.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/welcome").permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                .build();
+    }
+}
